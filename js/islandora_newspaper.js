d991a50ff8ddaadbbe8044ad2f7ff47c8cd209fe
DannyJoris <danny_joris@yahoo.co.uk>
2013-01-08 12:08:35 -0400

    added controls for issue paging on issues and pages

--- js/islandora_newspaper.js
+++ js/islandora_newspaper.js
@@ -4,13 +4,13 @@
  */
 
 (function ($) {
-  // Triggers a pager dropdown
-  Drupal.behaviors.islandora_newspaper_dropdown_pager = {
+  // Select page
+  Drupal.behaviors.islandoraNewspaperSelectPage = {
     attach: function(context, settings) {
       if (!$("#page-select").hasClass('processed')) {
         $("#page-select").change(function(e) {
           var pid = $("#page-select option:selected").attr('value');
-          window.location = Drupal.settings.basePath + 'islandora/object/' + pid;
+          window.location = Drupal.settings.basePath + 'islandora/object/' + pid; // check plain?
         });
         $("#page-select").addClass('processed');
       }
