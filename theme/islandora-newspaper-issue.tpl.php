bcd6012c95939ac18d8da5407038e1418adcd767
Yuqing Jiang <yqjiang0830@gmail.com>
2013-12-06 18:19:01 +0000

    Add IA viewer out put.

--- theme/islandora-newspaper-issue.tpl.php
+++ theme/islandora-newspaper-issue.tpl.php
@@ -5,8 +5,17 @@
  */
 ?>
 <div class="islandora-newspaper-issue clearfix">
-  <span class="islandora-newspaper-issue-navigator">
+  <?php
+  if ($viewer_id == 'islandora_internet_archive_bookreader') {
+    print $viewer;
+  }
+  else {
+    ?>
+      <span class="islandora-newspaper-issue-navigator">
     <?php print theme('islandora_newspaper_issue_navigator', array('object' => $object)); ?>
-  </span>
-  <?php print theme('islandora_objects', array('objects' => $pages)); ?>
+      </span>
+  <?php
+  print theme('islandora_objects', array('objects' => $pages));
+}
+?>
 </div>
