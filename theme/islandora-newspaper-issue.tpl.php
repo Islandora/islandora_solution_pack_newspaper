ed46f5d7ae9646310bc61e05b15c6cb21fdfcb86
Yuqing Jiang <yqjiang0830@gmail.com>
2013-12-11 14:30:02 +0000

    Add access function for page tab. Add view all issue when using IA viewer.

--- theme/islandora-newspaper-issue.tpl.php
+++ theme/islandora-newspaper-issue.tpl.php
@@ -5,17 +5,15 @@
  */
 ?>
 <div class="islandora-newspaper-issue clearfix">
+  <span class="islandora-newspaper-issue-navigator">
+    <?php print theme('islandora_newspaper_issue_navigator', array('object' => $object)); ?>
+  </span>
   <?php
   if ($viewer_id == 'islandora_internet_archive_bookreader') {
     print $viewer;
   }
   else {
-    ?>
-      <span class="islandora-newspaper-issue-navigator">
-    <?php print theme('islandora_newspaper_issue_navigator', array('object' => $object)); ?>
-      </span>
-  <?php
-  print theme('islandora_objects', array('objects' => $pages));
-}
-?>
+    print theme('islandora_objects', array('objects' => $pages));
+  }
+  ?>
 </div>
