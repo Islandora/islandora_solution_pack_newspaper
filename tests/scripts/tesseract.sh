08620a4a713f5900b3fda46cd0f968714b38a775
Diego Pino Navarro <dpino@metro.org>
2016-12-15 17:31:22 -0500

    Update tesseract in Travis
    To enable english trained data from Master branch in github to be used
    and avoid core dumps

--- tests/scripts/tesseract.sh
+++ tests/scripts/tesseract.sh
@@ -6,7 +6,7 @@ UBUNTUDIST="`lsb_release -sc`" # precise means from source
 CANRUN="1"
 if [ "$UBUNTUDIST" != "precise" ]; then
   echo "Installing Tesseract OCR using Ubuntu Packages"
-  apt-get --yes install tesseract-ocr tesseract-ocr-eng
+  apt-get --yes install tesseract-ocr tesseract-ocr-eng 
 fi
 # Check if installation worked or was already there
 $(command -v tesseract --version >/dev/null 2>&1 || exit 1)
@@ -20,18 +20,17 @@ if [ "$CANRUN" -eq "1" ]; then
   cd ~/tesseract
   printf "\n"
   echo "Installing Tesseract OCR from Source"
-  wget http://www.leptonica.org/source/leptonica-1.69.tar.gz
-  tar xf leptonica-1.69.tar.gz && rm -rf leptonica-1.69.tar.gz
-  cd leptonica-1.69
+  wget http://www.leptonica.org/source/leptonica-1.73.tar.gz
+  tar xf leptonica-1.73.tar.gz && rm -rf leptonica-1.73.tar.gz
+  cd leptonica-1.73
   ./configure
-  make && checkinstall --pkgname=libleptonica --pkgversion="1.69" --backup=no --deldoc=yes --fstrans=no --default
+  make && checkinstall --pkgname=libleptonica --pkgversion="1.73" --backup=no --deldoc=yes --fstrans=no --default
   cd ~/tesseract
-  wget https://github.com/tesseract-ocr/tesseract/archive/3.02.02.tar.gz
-  tar xf 3.02.02.tar.gz && rm -rf 3.02.02.tar.gz
-  cd tesseract-3.02.02
+  git clone https://github.com/tesseract-ocr/tesseract.git -b 3.04 3.04.01 --depth 1
+  cd 3.04.01
   ./autogen.sh
   ./configure
-  make && checkinstall --pkgname=tesseract-ocr --pkgversion="3.02.02" --backup=no --deldoc=yes --fstrans=no --default && ldconfig
+  make && checkinstall --pkgname=tesseract-ocr --pkgversion="3.04.01" --backup=no --deldoc=yes --fstrans=no --default && ldconfig
   mkdir ~/tesseract/langs
   cd ~/tesseract/langs
   wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.traineddata
@@ -41,5 +40,5 @@ if [ "$CANRUN" -eq "1" ]; then
 fi
 # If this fails, then we are out of luck
 printf "\n"
-echo "tessceract output:"
+echo "tesseract output:"
 tesseract --version && tesseract --list-langs
