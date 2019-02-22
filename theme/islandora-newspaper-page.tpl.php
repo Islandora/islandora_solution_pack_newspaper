717353d64e557d44cf945f0b8ae5cfcc0768d142
Jared Whiklo <jwhiklo@gmail.com>
2019-02-22 15:04:50 -0600

    Clean up for new code sniffs (#164)
    * Clean up for new code sniffs
    
    * Remove build.xml

--- theme/islandora-newspaper-page.tpl.php
+++ theme/islandora-newspaper-page.tpl.php
@@ -2,8 +2,7 @@
 
 /**
  * @file
- * islandora-newspaper-page.tpl.php
- * This is the template file for the object page for newspaper
+ * This is the template file for the object page for newspaper.
  *
  * Available variables:
  * - $object: The Islandora object rendered in this template file
@@ -15,7 +14,6 @@
  *
  * @see template_preprocess_islandora_newspaper_page()
  * @see theme_islandora_newspaper_page()
- *
  */
 ?>
 <div class="islandora-newspaper-object">
